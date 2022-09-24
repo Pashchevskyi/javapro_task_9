95b628d7a133fbba08bf7a512f975042a58ba078
Pavlo Pashchevskyi <googalltooth@gmail.com>
2022-09-25 00:54:00 +0300

    changed default value of file logger format

--- src/ua/ithillel/lms/javapro/pavlopashchevskyi/task9/logger/file/FileLoggerConfigurationLoader.java
+++ src/ua/ithillel/lms/javapro/pavlopashchevskyi/task9/logger/file/FileLoggerConfigurationLoader.java
@@ -61,7 +61,7 @@ public class FileLoggerConfigurationLoader extends LoggerConfigurationLoader {
             try {
               format = settingsValue[1];
             } catch (ArrayIndexOutOfBoundsException e) {
-              format = "^\\[[0-9]{4}-[0-1][0-9]-[0-3][0-9]\\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\\]\\[DEBUG|INFO|ERROR|TRACE\\]\\sСообщение:\\s\\[.*\\]$";
+              format = "^\\[[0-9]{4}-[0-1][0-9]-[0-3][0-9]\\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\\]\\[(DEBUG|INFO|ERROR|TRACE)\\]\\sСообщение:\\s\\[.*\\]\\n$";
             }
           }
           break;
